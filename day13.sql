c0136c45487ab8f92e983f72c477332b19476dca
MPhuong1499 <phuongdoan1499@gmail.com>
2025-04-06 19:16:37 +0700

    Update day13.sql

--- day13.sql
+++ day13.sql
@@ -1,4 +1,61 @@
+---1
+WITH count_duplicate AS 
+(SELECT company_id, title, description, COUNT(*)
+FROM job_listings
+GROUP BY company_id, title, description
+HAVING COUNT(*) > 1)
+SELECT COUNT(company_id) AS duplicate_companies 
+FROM count_duplicate;
 
+---2
+WITH cte AS ( 
+SELECT 
+category, 
+product, 
+SUM(spend) AS total_spend,
+RANK() OVER (
+        PARTITION BY category
+        ORDER BY SUM(spend) DESC
+    ) AS spending_rank
+FROM product_spend
+WHERE EXTRACT(YEAR FROM transaction_date) = 2022 
+GROUP BY category, product
+)
+SELECT category, product, total_spend
+FROM cte 
+WHERE spending_rank <= 2;
+
+---3
+WITH cte AS
+(SELECT policy_holder_id, COUNT(case_id)
+FROM callers
+GROUP BY policy_holder_id
+HAVING COUNT(case_id) >= 3)
+SELECT COUNT(*) AS policy_holder_count
+FROM cte;
+
+---4
+SELECT p.page_id
+FROM pages p
+LEFT JOIN page_likes pl
+ON p.page_id = pl.page_id
+WHERE pl.page_id IS NULL;
+
+---5
+WITH active_user_id AS
+(SELECT user_id
+FROM user_actions
+WHERE EXTRACT (MONTH FROM event_date) = 7
+GROUP BY user_id,EXTRACT (MONTH FROM event_date)
+INTERSECT
+SELECT user_id
+FROM user_actions
+WHERE EXTRACT (MONTH FROM event_date) = 6
+GROUP BY user_id,EXTRACT (MONTH FROM event_date))
+SELECT 7 AS month,
+COUNT(*) AS monthly_active_users
+FROM active_user_id;
+  
 ---6
 SELECT
 TO_CHAR(trans_date, 'yyyy-mm') AS month,
