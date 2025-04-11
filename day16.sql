67392d4636e9cb0e87ddcd0bb9d8dc0f395678f8
MPhuong1499 <phuongdoan1499@gmail.com>
2025-04-11 10:38:14 +0700

    Update day16.sql

--- day16.sql
+++ day16.sql
@@ -88,7 +88,23 @@ FROM cte
 SELECT
 visited_on, amount, average_amount
 FROM cte1 
-WHERE ranking > 6
+WHERE ranking > 6;
+
+---from discussion
+WITH last_6_days AS (
+    SELECT DISTINCT visited_on
+    FROM Customer
+    ORDER BY visited_on ASC
+    OFFSET 6
+)
+
+SELECT c1.visited_on,
+        SUM(c2.amount) AS amount,
+        ROUND(SUM(c2.amount) / 7., 2)  AS average_amount
+FROM last_6_days AS c1
+INNER JOIN Customer AS c2
+ON c2.visited_on BETWEEN c1.visited_on - 6 AND c1.visited_on
+GROUP BY c1.visited_on;
     
 ---5
 SELECT ROUND(SUM(tiv_2016)::NUMERIC,2) AS tiv_2016
